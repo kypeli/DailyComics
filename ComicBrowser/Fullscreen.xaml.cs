0f89acbba90dc3963903ed0069842c3b844fa2ed
Johan Paul <johan.paul@cybercom.com>
2012-05-09 13:54:00 +0300

    Add Fullscreen page to the project and load it when the comic is tapped.

---
+++ ComicBrowser/Fullscreen.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using ComicBrowser.ViewModels;
+using System.Diagnostics;
+using System.Windows.Media.Imaging;
+
+namespace ComicBrowser
+{
+    public partial class Fullscreen : PhoneApplicationPage
+    {
+        public Fullscreen()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+            if (this.NavigationContext.QueryString.ContainsKey("comicIndex"))
+            {
+                int comicIndex = Convert.ToInt16(this.NavigationContext.QueryString["comicIndex"]);
+                if (comicIndex >= 0)
+                {
+                    BitmapImage bmImage = App.comicListModel.ShowingComicsListModel.ElementAt(comicIndex).ComicImage;
+                    FullscreenComic.Source = bmImage;
+                }
+            }
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}    
