f3919181770d207dd1333dd18d9da29a7750ed55
Johan Paul <johan.paul@gmail.com>
2012-06-04 15:10:43 +0300

    Added a more interesting license text to iOS files.

--- iOS/Daily Comics/DCComicListViewController.m
+++ iOS/Daily Comics/DCComicListViewController.m
@@ -1,10 +1,30 @@
-//
-//  DCMainViewController.m
-//  Daily Comics
-//
-//  Created by Johan Paul on 5/24/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
+/**
+ * Copyright (c) 2012, Johan Paul <johan.paul@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.using System;
+ */
+
 #import <Foundation/NSJSONSerialization.h> 
 
 #import "DCComicListViewController.h"
@@ -128,7 +148,6 @@
     
     // Store the JSON list as shared data to the App delegate. The Settings view might want to re-use it.
     appDelegate.comicListJson = [json objectForKey:@"comics"];
-    // comicsListModel = [appDelegate comicListModel:YES];
     
     NSLog(@"Got comics, %u items.", [appDelegate.comicListJson count]);
     
@@ -189,7 +208,9 @@
     }
 }
 
+
 // Add comic -button handler.
+
 - (void)addComic:(id)sender {
     NSLog(@"Add comics.");
     
@@ -198,7 +219,8 @@
     [self.navigationController pushViewController:addComics animated:YES];
 }
 
-/* NSFetchedResultsControllerDelegate implementations. */
+
+// NSFetchedResultsControllerDelegate implementations.
 
 - (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject
        atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type
